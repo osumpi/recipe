edeedf38b12aec216359fd899029f096580be813
Rithvik Nishad <rithvikn2001@gmail.com>
2021-04-06 14:59:03 +0530

    BakeState as Enum and removed BakeResult

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,42 +1,56 @@
 part of recipe;
 
+enum BakeState {
+  Ready,
+  Running,
+  Paused,
+  Abortive,
+  CompletedSuccesfully,
+  CompletedPartially,
+  CompletedUnsuccessfully,
+}
+
+extension _MultiBakeStateEvaluator on BakeState {
+  static BakeState combineFrom(Iterable<BakeState> states) {
+    return BakeState.Ready;
+  }
+}
+
 class BakeContext {
   const BakeContext();
 }
 
-@immutable
-class BakeResult {
-  static const Unknown = BakeResult._('Unknown');
-  static const Success = BakeResult._('Success');
-  static const Failed = BakeResult._('Failed');
-  static const PartiallyFailed = BakeResult._('PartiallyFailed');
+// @immutable
+// class BakeResult {
+//   static const Unknown = BakeResult._('Unknown');
+//   static const Success = BakeResult._('Success');
+//   static const Failed = BakeResult._('Failed');
+//   static const PartiallyFailed = BakeResult._('PartiallyFailed');
 
-  static const values = [Unknown, Success, Failed, PartiallyFailed];
+//   static const values = [Unknown, Success, Failed, PartiallyFailed];
 
-  const BakeResult._(final String value) : _value = value;
+//   const BakeResult._(final String value) : _value = value;
 
-  factory BakeResult.combineFrom(Iterable<BakeResult> results) {
-    try {
-      return Set.from(results).single;
-    } on StateError {
-      return BakeResult.PartiallyFailed;
-    }
-  }
+//   factory BakeResult.combineFrom(Iterable<BakeResult> results) {
+//     try {
+//       return Set.from(results).single;
+//     } on StateError {
+//       return BakeResult.PartiallyFailed;
+//     }
+//   }
 
-  final String _value;
+//   final String _value;
 
-  @override
-  String toString() => 'BakeResult.$_value';
-}
+//   @override
+//   String toString() => 'BakeResult.$_value';
+// }
 
 extension RecipeListExtension on Iterable<Recipe> {
-  void bakeParallel(BakeContext context) =>
-      BakeController.parallel(this).bake(context);
+  void bakeParallel(BakeContext context) => Baker.parallel(this).bake(context);
 
-  void bakeSeries(BakeContext context) =>
-      BakeController.sequential(this).bake(context);
+  void bakeSeries(BakeContext context) => Baker.sequential(this).bake(context);
 }
 
-Future<BakeResult> bake(Recipe recipe) {
+Future<BakeState> bake(Recipe recipe) {
   return recipe.bake(BakeContext());
 }
