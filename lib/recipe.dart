5c7d255e16f0a40e04d0bc38f7a5446f195e2012
Rithvik Nishad <rithvikn2001@gmail.com>
2021-04-11 16:16:01 +0530

    Adds Key

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -3,8 +3,10 @@ library recipe;
 import 'dart:async';
 
 import 'package:checked_yaml/checked_yaml.dart';
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:meta/meta.dart';
+import 'package:uuid/uuid.dart';
 
 import 'src/bake_state.dart' show BakeState;
 export 'src/bake_state.dart' show BakeState;
@@ -13,5 +15,6 @@ part 'src/parsed_recipe.dart';
 part 'src/bake_context.dart';
 part 'src/bakers.dart';
 part 'src/utils.dart';
+part 'src/key.dart';
 
 part 'recipe.g.dart';
