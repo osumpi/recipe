032c4eb909eb5f2292837cf4e3b1d0ac7d73e564
Rithvik Nishad <rithvikn2001@gmail.com>
2021-04-11 21:45:09 +0530

    recipe.dart

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -3,18 +3,15 @@ library recipe;
 import 'dart:async';
 
 import 'package:checked_yaml/checked_yaml.dart';
-import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:meta/meta.dart';
-import 'package:uuid/uuid.dart';
 
-import 'src/bake_state.dart' show BakeState;
-export 'src/bake_state.dart' show BakeState;
+part 'src/bake_state.dart';
 part 'src/recipe.dart';
+part 'src/recipe_driver.dart';
 part 'src/parsed_recipe.dart';
 part 'src/bake_context.dart';
 part 'src/bakers.dart';
 part 'src/utils.dart';
-part 'src/key.dart';
 
 part 'recipe.g.dart';
