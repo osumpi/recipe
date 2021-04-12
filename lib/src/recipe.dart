cd96a6cb925e7836d944741484c1c4be58ddbfbf
Rithvik Nishad <rithvikn2001@gmail.com>
2021-04-12 20:35:19 +0530

    Adds doc-comments to Recipe

--- lib/src/recipe.dart
+++ lib/src/recipe.dart
@@ -1,16 +1,26 @@
 part of recipe;
 
+/// This class implements the abstract model of [Recipe].
+///
+/// In BakeCode, a custom [Recipe] may be implemented using one more [Recpie]s.
+/// [Recipe] provides the required abstraction for performing [Recipe.bake]
+/// while being able to work with the BakeCode Ecosystem.
 abstract class Recipe {
+  /// The name of this [Recipe].
   String get name;
 
+  /// The author of this [Recipe].
   String get author;
 
+  /// The version of this [Recipe].
   String get version;
 
+  /// The description of this [Recipe].
   String get description;
 
   const Recipe();
 
+  /// Parse [Recipe] from YAML source.
   factory Recipe.parse(String source, {Uri? url}) {
     Recipe fromMap(map) {
       if (map == null) {
@@ -23,19 +33,19 @@ abstract class Recipe {
     return checkedYamlDecode(source, fromMap, sourceUrl: url, allowNull: true);
   }
 
-  // @protected
+  /// Bake's the [Recipe].
+  /// [context] is the [BakeContext] provided by [RecipeDriver.drive] method.
+  ///
+  /// [bake] shall not be invoked, other than by [RecipeDriver].
+  ///
+  /// **Returns**
+  /// [Stream] of [BakeState].
+  @protected
   Stream<BakeState> bake(BakeContext context);
 
+  /// The string representation of the [Recipe] instance.
+  ///
+  /// Format: `Recipe($name $version)`
   @override
-  String toString() => 'Instance of Recipe($name $version)';
-}
-
-abstract class _InbuiltRecipe extends Recipe {
-  const _InbuiltRecipe();
-
-  @nonVirtual
-  String get author => 'bakecode-devs';
-
-  @nonVirtual
-  String get version => 'bakecode:core';
+  String toString() => 'Recipe($name $version)';
 }
