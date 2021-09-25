8fabbae586aa44410faf5da6ab173e10bdc476a5
Rithvik Nishad <rithvikn2001@gmail.com>
2021-09-25 08:10:44 +0530

    update

--- example/recipe_example.dart
+++ example/recipe_example.dart
@@ -1,35 +1,59 @@
-import 'package:recipe/recipe.dart';
+/// From Package: https://github.com/alexei-sintotski/json2yaml
+/// https://github.com/alexei-sintotski/json2yaml/blob/master/example/main.dart
 
-main() => bake(Coffee());
+import 'package:fhir_yaml/fhir_yaml.dart';
 
-@PublicRecipe(name: 'Coffee')
-class Coffee extends Recipe {
-  bake(context) async* {
-    yield Provider.value(
-      // Wait until you get a coffee cup
-      await CoffeeCup.request(),
+// ignore_for_file: avoid_print
 
-      // then runs the following.
-      recipe: Parallel([
-        DispenseSugar('2 teaspoon'),
-        DispenseCoffeePowder('1 teaspoon'),
-      ]),
-      // TODO: add builder and recipe
-    );
-  }
+void main() {
+  const developerData = {
+    'name': "Martin D'vloper",
+    'job': 'Developer',
+    'skill': 'Elite',
+    'employed': true,
+    'foods': ['Apple', 'Orange', 'Strawberry', 'Mango'],
+    'languages': {
+      'perl': 'Elite',
+      'python': 'Elite',
+      'pascal': 'Lame',
+    },
+    'education': '4 GCSEs\n3 A-Levels\nBSc in the Internet of Things'
+  };
+
+  print(json2yaml(developerData));
 }
+// import 'package:recipe/recipe.dart';
 
-class CoffeeCup {
-  static Future<CoffeeCup> request() async => CoffeeCup();
-}
+// main() => bake(Coffee());
 
-class DispenseSugar extends Recipe {
-  const DispenseSugar(String qty);
+// @PublicRecipe(name: 'Coffee')
+// class Coffee extends Recipe {
+//   bake(context) async* {
+//     yield Provider.value(
+//       // Wait until you get a coffee cup
+//       await CoffeeCup.request(),
 
-  bake(context) async* {}
-}
+//       // then runs the following.
+//       recipe: Parallel([
+//         DispenseSugar('2 teaspoon'),
+//         DispenseCoffeePowder('1 teaspoon'),
+//       ]),
+//       // TODO: add builder and recipe
+//     );
+//   }
+// }
 
-class DispenseCoffeePowder extends Recipe {
-  const DispenseCoffeePowder(String qty);
-  bake(context) async* {}
-}
+// class CoffeeCup {
+//   static Future<CoffeeCup> request() async => CoffeeCup();
+// }
+
+// class DispenseSugar extends Recipe {
+//   const DispenseSugar(String qty);
+
+//   bake(context) async* {}
+// }
+
+// class DispenseCoffeePowder extends Recipe {
+//   const DispenseCoffeePowder(String qty);
+//   bake(context) async* {}
+// }
