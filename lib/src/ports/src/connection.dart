f8729a53058f319a5e1ba5914f5847976c03445a
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-31 23:31:09 +0530

    connection

---
+++ lib/src/ports/src/connection.dart
@@ -0,0 +1,49 @@
+part of recipe.ports;
+
+class Connection<T extends BakeContext> with FrameworkEntity {
+  Connection({
+    required this.from,
+    required this.to,
+  }) {
+    SketchRegistry.registerConnection(this);
+  }
+
+  factory Connection.wireless({
+    required OutputPort<T> from,
+    required InputPort<T> to,
+  }) = WirelessConnection;
+
+  final OutputPort<T> from;
+  final InputPort<T> to;
+
+  final isWireless = false;
+
+  void write(T context) {
+    to.events.add(context);
+  }
+
+  @override
+  JsonMap toJson() {
+    return {
+      'from': from.hashCode,
+      'to': to.hashCode,
+      'is wireless': isWireless,
+      'context type': T.toString(),
+    };
+  }
+
+  Connection<T> get asWirelessConnection =>
+      Connection.wireless(from: from, to: to);
+}
+
+class WirelessConnection<T extends BakeContext> extends Connection<T> {
+  WirelessConnection({
+    required OutputPort<T> from,
+    required InputPort<T> to,
+  }) : super(from: from, to: to);
+
+  final isWireless = true;
+
+  @override
+  Connection<T> get asWirelessConnection => this;
+}
