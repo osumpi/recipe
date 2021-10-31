d2969eb0d5a376eb2bc51153e3d0bfb843efbfdb
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-31 23:31:28 +0530

    bake context

---
+++ lib/src.backup/bake_context.dart
@@ -0,0 +1,11 @@
+part of recipe.old;
+
+class BakeContext with _RecipeFrameworkEntity {
+  @override
+  JsonMap toJson() {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
+}
+
+class _CloseWireContext extends BakeContext {}
