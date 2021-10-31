b88813183687377788b28336ba3847e56d441207
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-31 23:28:14 +0530

    recipe implementation

--- lib/src/recipe.dart
+++ lib/src/recipe.dart
@@ -1,59 +1,39 @@
-part of recipe;
+import 'dart:async';
 
-typedef RecipeStream = Stream<Recipe>;
+import 'package:meta/meta.dart';
 
-/// Annotate a recipe with this class to be used as a constructable recipe
-/// inside OsumPie's recipe maker. Shall be annoted only on [IRecipe]
-/// implementations.
-///
-/// Example:
-/// TODO: Write Recipe meta usage example.
-/// ```
-@immutable
-class PublicRecipe {
-  /// Name of the [Recipe] that will be shown in OsumPie's recipe editor.
-  /// All public recipe's shall define this with a meaningful name.
-  final String name;
+import 'package:recipe/src/utils.dart';
+import 'package:recipe/src/bake_context.dart';
+import 'package:recipe/src/io_mixins.dart';
+import 'package:recipe/src/sketch_registry.dart';
 
-  /// The authors of [Recipe].
-  final String description;
+abstract class Recipe<I extends BakeContext, O extends BakeContext>
+    with FrameworkEntity, InputMixin<I>, OutputMixin<O> {
+  bool isInitialized = false;
 
-  /// The description of the [Recipe].
-  final Iterable<String> authors;
+  /// Initializes the recipe.
+  @mustCallSuper
+  void initialize({
+    bool allowMultipleInputs = true,
+  }) {
+    if (isInitialized) {
+      warn('Already initialized');
+    }
 
-  /// Annotate a recipe with this class to be used as a constructable recipe
-  /// inside OsumPie's recipe maker. Shall be annoted only on [Recipe]
-  /// implementations.
-  const PublicRecipe({
-    this.name = 'No name',
-    this.description = 'No description',
-    this.authors = const [],
-  });
+    SketchRegistry.registerRecipe(this);
 
-  const PublicRecipe._core({
-    required this.name,
-    required this.description,
-  }) : this.authors = _coreAuthors;
+    isInitialized = true;
+  }
 
-  static const _coreAuthors = ['BakeCode Core'];
-}
-
-/// This class implements the abstract model of [Recipe].
-///
-/// In BakeCode, a custom [Recipe] may be implemented using one more [Recpie]s.
-/// [Recipe] provides the required abstraction for performing [Recipe.bake]
-/// in the BakeCode Ecosystem.
-abstract class Recipe {
-  String get name => '$runtimeType';
-
-  const Recipe();
-
-  /// Bake's the [Recipe].
-  /// [context] is the [BakeContext] provided by [RecipeDriver.drive] method.
-  ///
-  /// [bake] shall not be invoked, other than by [RecipeDriver].
-  ///
-  /// Returns [RecipeStream] that may yield multiple, single or no recipes.
   @protected
-  RecipeStream bake(BakeContext context);
+  Stream<O> bake(I context);
+
+  @override
+  JsonMap toJson() {
+    return {
+      'recipe': runtimeType.toString(),
+      'input port': inputPort.hashCode,
+      'output port': outputPort.hashCode,
+    };
+  }
 }
