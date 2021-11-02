39196baf22465b7500bd73291f04653b46e13453
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-02 21:55:20 +0530

    utils implement FrameworkUtils for logging

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,50 +1,28 @@
 import 'dart:io';
 
-import 'package:fhir_yaml/fhir_yaml.dart';
 import 'package:meta/meta.dart';
-import 'package:recipe/recipe.dart';
-import 'package:recipe/src/ports/ports.dart';
 
-mixin FrameworkEntity {
-  String get name => runtimeType.toString();
-
-  @protected
-  void log(
+@sealed
+abstract class FrameworkUtils {
+  static void log(
     String message, {
+    required String module,
     String level = 'I',
   }) {
-    stdout.writeln('$level/$name#$hashCode: $message');
+    stdout.writeln('$level/$module: $message');
   }
 
-  @protected
-  void warn(String message) => log(message, level: 'W');
+  static void warn(String message, {required String module}) =>
+      log(message, module: module, level: 'W');
 
-  @protected
-  void error(String message) => log(message, level: 'E');
+  static void error(String message, {required String module}) =>
+      log(message, module: module, level: 'E');
 
-  @protected
-  void info(String message) => log(message, level: 'I');
+  static void info(String message, {required String module}) =>
+      log(message, module: module, level: 'I');
 
-  @protected
-  void verbose(String message) => log(message, level: 'V');
-
-  JsonMap toJson();
-
-  @nonVirtual
-  String toYaml() => json2yaml(toJson());
-
-  @protected
-  @mustCallSuper
-  bool register() {
-    final entity = this;
-
-    if (entity is Recipe) return SketchRegistry.recipes.add(entity);
-    if (entity is Port) return SketchRegistry.ports.add(entity);
-    if (entity is Connection) return SketchRegistry.connections.add(entity);
-
-    throw UnsupportedError(
-        '${this.runtimeType} is not a supported type that can be registered.');
-  }
+  static void verbose(String message, {required String module}) =>
+      log(message, module: module, level: 'V');
 }
 
 typedef JsonMap = Map<String, dynamic>;
