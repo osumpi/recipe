e179e6a38c5fecf753e1090096ac48b7175efb12
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-02 23:10:09 +0530

    support for log levels

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -2,27 +2,58 @@ import 'dart:io';
 
 import 'package:meta/meta.dart';
 
+class LogLevel implements Comparable<LogLevel> {
+  @internal
+  const LogLevel(this.name, this.value);
+
+  final int value;
+  final String name;
+
+  @override
+  int compareTo(LogLevel other) => value.compareTo(other.value);
+
+  @override
+  String toString() => name;
+}
+
+@sealed
+abstract class LogLevels {
+  static const off = LogLevel('off', 100);
+  static const fatal = LogLevel('fatal', 70);
+  static const error = LogLevel('error', 60);
+  static const warning = LogLevel('warning', 50);
+  static const info = LogLevel('info', 40);
+  static const debug = LogLevel('debug', 30);
+  static const verbose = LogLevel('verbose', 20);
+  static const trace = LogLevel('trace', 10);
+  static const all = LogLevel('all', 0);
+}
+
 @sealed
 abstract class FrameworkUtils {
+  static LogLevel loggingLevel = LogLevels.all;
+
   static void log(
     String message, {
     required String module,
-    String level = 'I',
+    LogLevel level = LogLevels.info,
   }) {
-    stdout.writeln('$level/$module: $message');
+    if (level.value < loggingLevel.value) return;
+
+    stdout.writeln('${level.name[0]}/$module: $message');
   }
 
   static void warn(String message, {required String module}) =>
-      log(message, module: module, level: 'W');
+      log(message, module: module, level: LogLevels.warning);
 
   static void error(String message, {required String module}) =>
-      log(message, module: module, level: 'E');
+      log(message, module: module, level: LogLevels.error);
 
   static void info(String message, {required String module}) =>
-      log(message, module: module, level: 'I');
+      log(message, module: module);
 
   static void verbose(String message, {required String module}) =>
-      log(message, module: module, level: 'V');
+      log(message, module: module, level: LogLevels.verbose);
 }
 
 typedef JsonMap = Map<String, dynamic>;
