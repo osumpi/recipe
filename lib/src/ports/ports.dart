8ef4c95ae61fdc5d751a2551152ee0f81e2f65d7
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-02 21:54:59 +0530

    ports use new framework_entity import

--- lib/src/ports/ports.dart
+++ lib/src/ports/ports.dart
@@ -5,7 +5,7 @@ import 'dart:async';
 import 'package:meta/meta.dart';
 import 'package:recipe/src/utils.dart';
 import 'package:recipe/src/bake_context.dart';
-
+import 'package:recipe/src/framework_entity.dart';
 part 'src/input_port.dart';
 part 'src/output_port.dart';
 part 'src/port_base.dart';
