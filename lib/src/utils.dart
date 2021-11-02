22c6512cdbb695dbae3f016bef42a9392589cda5
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-02 17:32:48 +0530

    utils support entity registration and impl. toYaml

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:fhir_yaml/fhir_yaml.dart';
 import 'package:meta/meta.dart';
+import 'package:recipe/recipe.dart';
+import 'package:recipe/src/ports/ports.dart';
 
 mixin FrameworkEntity {
   String get name => runtimeType.toString();
@@ -23,12 +25,25 @@ mixin FrameworkEntity {
   @protected
   void info(String message) => log(message, level: 'I');
 
+  @protected
   void verbose(String message) => log(message, level: 'V');
 
   JsonMap toJson();
 
-  String toYaml() {
-    return json2yaml(toJson());
+  @nonVirtual
+  String toYaml() => json2yaml(toJson());
+
+  @protected
+  @mustCallSuper
+  bool register() {
+    final entity = this;
+
+    if (entity is Recipe) return SketchRegistry.recipes.add(entity);
+    if (entity is Port) return SketchRegistry.ports.add(entity);
+    if (entity is Connection) return SketchRegistry.connections.add(entity);
+
+    throw UnsupportedError(
+        '${this.runtimeType} is not a supported type that can be registered.');
   }
 }
 
