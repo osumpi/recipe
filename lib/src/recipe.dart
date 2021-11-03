5466161a16fdecaf019ed24d50fb0b7969be6380
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-03 13:04:39 +0530

    remove recipe in/out type

--- lib/src/recipe.dart
+++ lib/src/recipe.dart
@@ -4,13 +4,12 @@ import 'package:recipe/src/bake_context.dart';
 import 'package:recipe/src/framework_entity.dart';
 import 'package:recipe/src/utils.dart';
 
-abstract class Recipe<In extends BakeContext, Out extends BakeContext>
-    with FrameworkEntity, EntityLogging {
+abstract class Recipe with FrameworkEntity, EntityLogging {
   const Recipe();
 
   @protected
   @mustCallSuper
-  Stream<Out> bake(In context);
+  Stream<BakeContext> bake(BakeContext context);
 
   @override
   JsonMap toJson() {
@@ -21,11 +20,12 @@ abstract class Recipe<In extends BakeContext, Out extends BakeContext>
 }
 
 class Baker {
-  const Baker.of(BakeContext context) : parentContext = context;
+  const Baker.of(BakeContext? context) : parentContext = context;
 
-  final BakeContext parentContext;
+  @internal
+  final BakeContext? parentContext;
 
-  Stream bake(Recipe recipe) async* {
+  Stream<BakeContext> bake(Recipe recipe) async* {
     final childContext = BakeContext(recipe, parentContext);
 
     yield* recipe.bake(childContext);
