a9fe8da37bc9cf956400cf88994423f259a7dfbe
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-03 11:51:40 +0530

    impl. new recipe structure

--- lib/src/recipe.dart
+++ lib/src/recipe.dart
@@ -1,49 +1,21 @@
-import 'dart:async';
-
 import 'package:meta/meta.dart';
 
-import 'package:recipe/src/utils.dart';
 import 'package:recipe/src/bake_context.dart';
-import 'package:recipe/src/ports/ports.dart';
 import 'package:recipe/src/framework_entity.dart';
+import 'package:recipe/src/utils.dart';
 
-abstract class Recipe<Input extends BakeContext, Output extends BakeContext>
-    with FrameworkEntity {
-  InputPort<Input> get inputPort;
-  OutputPort<Output> get outputPort;
-
-  Recipe() {
-    register();
-  }
-
-  StreamSubscription<Input>? _subscription;
-
-  void initialize() {
-    if (_subscription != null) {
-      return error('Failed to initialize recipe $name.');
-    }
-
-    _subscription = inputPort.events.stream.listen((context) {
-      bake(context).listen(outputPort.write);
-    });
-
-    verbose('Recipe "$name" initialized.');
-  }
-
-  void dispose() {
-    _subscription?.cancel();
-    verbose('Recipe "$name" diposed');
-  }
+abstract class Recipe<In extends BakeContext, Out extends BakeContext>
+    with FrameworkEntity, EntityLogging {
+  const Recipe();
 
   @protected
-  Stream<Output> bake(Input context);
+  @mustCallSuper
+  Stream<Out> bake(In context);
 
   @override
   JsonMap toJson() {
     return {
-      'recipe': runtimeType.toString(),
-      'input port': inputPort.hashCode,
-      'output port': outputPort.hashCode,
+      ...super.toJson(),
     };
   }
 }
