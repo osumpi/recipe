c6609a0bcd21e04b8256b5fff55b50e8231da9a4
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-04 19:01:50 +0530

    updates test to demonstrate using new status decorations

--- example/test.dart
+++ example/test.dart
@@ -1,9 +1,10 @@
 import 'package:recipe/recipe.dart';
 import 'package:recipe/src/recipe.dart';
+import 'package:recipe/src/utils.dart';
 
 void main() async {
   FrameworkUtils.loggingLevel = LogLevels.verbose;
-  FrameworkUtils.showTimestampInLogs = false;
+  FrameworkUtils.showTimestampInLogs = true;
 
   bake(MyRecipe()).listen(FrameworkUtils.log);
 }
@@ -15,22 +16,24 @@ class MyRecipe extends Recipe {
 
     final baker = Baker.of(context);
 
-    status('Just one recipe.');
+    status('Just one recipe.', status: Statuses.successful);
     yield* baker.bake(RecipeB());
 
-    status('Multiple recipes one by one (sequential)');
+    status('Multiple recipes one by one (sequential)', status: Statuses.failed);
     yield* baker.bake(RecipeB());
     yield* baker.bake(Delayed(RecipeB(), duration: const Duration(seconds: 1)));
     yield* baker.bake(RecipeB());
 
-    status('Multiple recipes in sequential using Baker and BakeStrategy');
+    status('Multiple recipes in sequential using Baker and BakeStrategy',
+        status: Statuses.warning);
     yield* baker.bakeAll([
       RecipeB(),
       Delayed(RecipeB(), duration: const Duration(seconds: 1)),
       RecipeB(),
     ]);
 
-    status('Multiple recipes in parallel using Baker and BakeStrategy');
+    status('Multiple recipes in parallel using Baker and BakeStrategy',
+        status: Statuses.successful);
     yield* baker.bakeAll(
       [
         RecipeB(),
