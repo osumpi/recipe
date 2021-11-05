2dcbaa4b22e4a5e6e00164edd1cd032c62969c90
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-05 20:52:47 +0530

    library bakers

---
+++ lib/src/bakers/bakers.dart
@@ -0,0 +1,21 @@
+library recipe.bakers;
+
+import 'package:meta/meta.dart';
+
+import 'package:recipe/src/baker.dart' show Baker;
+import 'package:recipe/src/bake_context.dart' show BakeContext;
+import 'package:recipe/src/utils.dart' show uuid;
+
+part 'src/concurrent.dart';
+
+class ConcurrentBaker = Baker with _ConcurrentBakeHandler;
+abstract class NonConcurrentBaker = Baker with _NonConcurrentBakerMixin;
+// class FirstInFirstOutBaker = NonConcurrentBaker with _FIFOBakeHandler;
+// class AngryBaker = NonConcurrentBaker with _AngryBakeHandler;
+// class SingleRunBaker = NonConcurrentBaker with _SingleRunBakeHandler;
+// class IncapacitatedConcurrentBaker = ConcurrentBaker with IncapacitatedBaker;
+// class IncapacitatedFIFOBaker = FirstInFirstOutBaker with IncapacitatedBaker;
+// class LazyBaker = FirstInFirstOutBaker with _LazyBakeHandler;
+// class RandomBaker = NonConcurrentBaker with _RandomBakeHandler;
+// class LimitedConcurrentBaker = ConcurrentBaker
+//     with _LimitedConcurrencyBakeHandler;
