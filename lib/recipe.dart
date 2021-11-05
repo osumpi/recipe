925ca33fb4994bf2f2e8d24826720eebe529661d
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-05 20:56:20 +0530

    update exports of `reicpe`

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,5 +1,7 @@
 library recipe;
 
 export 'src/bake_context.dart' show BakeContext;
-export 'src/recipe.dart' show Recipe, Baker, BakeStrategy;
+export 'src/recipe.dart' show Recipe, BakeStrategy;
+export 'src/baker.dart' show Baker, BakerStatus;
+export 'src/bake_report.dart' show BakeReport;
 export 'src/utils.dart' show bake, FrameworkUtils, LogLevels, Statuses;
