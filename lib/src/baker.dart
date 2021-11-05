080c7a631ecf9ff99224f0ed3b41518c374c3200
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-05 20:46:39 +0530

    implement `Baker` and `BakerStatus`

---
+++ lib/src/baker.dart
@@ -0,0 +1,48 @@
+import 'package:meta/meta.dart';
+
+import 'package:recipe/src/bake_report.dart' show BakeReport;
+import 'package:recipe/src/recipe.dart' show Recipe;
+import 'package:recipe/src/bake_context.dart' show BakeContext;
+import 'package:recipe/src/utils.dart';
+
+// TODO: export
+enum BakerStatus { baking, idle }
+
+abstract class Baker {
+  Baker(this.recipe);
+
+  @nonVirtual
+  final Recipe recipe;
+
+  bool get concurrencyAllowed;
+
+  @nonVirtual
+  final List<BakeReport> bakeLog = [];
+
+  @nonVirtual
+  int get bakesCompleted => bakeLog.length;
+
+  @protected
+  @nonVirtual
+  final uptimeStopwatch = Stopwatch();
+
+  @nonVirtual
+  BakerStatus get status =>
+      uptimeStopwatch.isRunning ? BakerStatus.baking : BakerStatus.idle;
+
+  @nonVirtual
+  bool get isBaking => status == BakerStatus.baking;
+
+  @nonVirtual
+  bool get isIdle => status == BakerStatus.idle;
+
+  @nonVirtual
+  Duration get totalUptime => uptimeStopwatch.elapsed;
+
+  bool get canBake;
+
+  @protected
+  Stream<BakeContext> bake(BakeContext inputContext);
+
+  void requestBake(BakeContext inputContext);
+}
