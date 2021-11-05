b2b74070cc4b1239d376e7f1fd47c39a93df1286
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-05 20:52:54 +0530

    adds concurrent baker

---
+++ lib/src/bakers/src/concurrent.dart
@@ -0,0 +1,43 @@
+part of recipe.bakers;
+
+mixin _ConcurrentBakeHandler on Baker {
+  @override
+  @nonVirtual
+  final concurrencyAllowed = true;
+
+  final Map<String, DateTime> beingBaked = {};
+
+  @override
+  Stream<BakeContext> bake(BakeContext inputContext) async* {
+    uptimeStopwatch.start();
+
+    final key = uuid.v4();
+    beingBaked[key] = DateTime.now();
+
+    yield* recipe.bake(inputContext);
+
+    beingBaked.remove(key)!;
+  }
+
+  @override
+  bool get canBake => true;
+
+  @override
+  void requestBake(BakeContext inputContext) {
+    bake(inputContext);
+  }
+}
+
+mixin _NonConcurrentBakerMixin on Baker {
+  @override
+  @nonVirtual
+  final concurrencyAllowed = false;
+}
+
+// mixin _LazyBakeHandler on FirstInFirstOutBaker {}
+// mixin _RandomBakeHandler on NonConcurrentBaker {}
+
+// mixin IncapacitatedBaker on Baker {}
+// mixin _FIFOBakeHandler on NonConcurrentBaker {}
+// mixin _AngryBakeHandler on NonConcurrentBaker {}
+// mixin _SingleRunBakeHandler on NonConcurrentBaker {}
