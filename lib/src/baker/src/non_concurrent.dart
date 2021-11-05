77db0871efd7fb03db37c332f8dba1f50e648dc5
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-05 22:37:55 +0530

    non concurrent baker improve api

--- lib/src/baker/src/non_concurrent.dart
+++ lib/src/baker/src/non_concurrent.dart
@@ -4,4 +4,18 @@ mixin _NonConcurrentBakerMixin on Baker {
   @override
   @nonVirtual
   final concurrencyAllowed = false;
+
+  @override
+  bool get canBake => isIdle;
+
+  @override
+  void requestBake(BakeContext inputContext) {
+    if (canBake) {
+      bake(inputContext);
+    } else {
+      handleBakeRequestWhenBaking(inputContext);
+    }
+  }
+
+  void handleBakeRequestWhenBaking(BakeContext inputContext);
 }
