04be479ecdbdbb82df23300bceddc898a593bc5b
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-05 22:48:35 +0530

    bake report include `bakedBy`

--- lib/src/bake_report.dart
+++ lib/src/bake_report.dart
@@ -1,17 +1,21 @@
 import 'package:meta/meta.dart';
 
-import 'package:recipe/src/bake_context.dart';
+import 'package:recipe/src/bake_context.dart' show BakeContext;
+import 'package:recipe/src/baker/baker.dart' show Baker;
 
 @immutable
 class BakeReport {
   BakeReport({
     required this.bakeId,
+    required this.bakedBy,
     required this.startedOn,
     required this.stoppedOn,
     required this.inputContext,
   });
 
   final String bakeId;
+
+  final Baker bakedBy;
 
   final DateTime startedOn;
 
@@ -24,6 +28,7 @@ class BakeReport {
   Map<String, dynamic> toJson() {
     return {
       'bake id': bakeId,
+      'baked by': '${bakedBy.runtimeType}#${bakedBy.hashCode}',
       'started on': startedOn,
       'stopped on': stoppedOn,
       'duration': duration,
