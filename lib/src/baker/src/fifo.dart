1fe483eab56081aea170f30780ee3d6ee8467caa
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-05 22:37:30 +0530

    fifo baker

---
+++ lib/src/baker/src/fifo.dart
@@ -0,0 +1,43 @@
+part of recipe.baker;
+
+mixin _FIFOBakeHandler on NonConcurrentBaker {
+  final requests = ListQueue<BakeContext>();
+
+  @override
+  Stream<BakeContext> bake(BakeContext inputContext) async* {
+    uptimeStopwatch.start();
+
+    final startedOn = DateTime.now();
+    final key = uuid.v4();
+
+    yield* recipe.bake(inputContext);
+
+    final report = BakeReport(
+      bakeId: key,
+      startedOn: startedOn,
+      stoppedOn: DateTime.now(),
+      inputContext: inputContext,
+    );
+
+    bakeLog.add(report);
+
+    uptimeStopwatch.stop();
+
+    tryCompletePendingRequests();
+  }
+
+  @override
+  void handleBakeRequestWhenBaking(BakeContext inputContext) {
+    requests.add(inputContext);
+  }
+
+  /// Voluntarily marked as future to allow requesting bake stream to close
+  /// without waiting for this to complete.
+  ///
+  /// Do not await this future inside [bake].
+  Future<void> tryCompletePendingRequests() async {
+    if (requests.isNotEmpty) {
+      bake(requests.removeFirst());
+    }
+  }
+}
