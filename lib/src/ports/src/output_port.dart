936961774f2ca50ffc1ddecb361a8fd93de555ba
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-06 16:10:15 +0530

    stronger output port

--- lib/src/ports/src/output_port.dart
+++ lib/src/ports/src/output_port.dart
@@ -3,11 +3,19 @@ part of recipe.ports;
 class OutputPort<T extends Object> extends Port<T> {
   OutputPort(String name) : super(name);
 
-  Set<Connection<T>> get connections => outboundConnections;
+  UnmodifiableSetView<Connection<T>> get connections =>
+      UnmodifiableSetView(outboundConnections);
 
+  @internal
   final outboundConnections = <Connection<T>>{};
 
   Connection<T>? connectTo(InputPort<T> inputPort, {bool wireless = false}) {
-    return inputPort.connectFrom(this, wireless: wireless);
+    final connection = inputPort.connectFrom(this, wireless: wireless);
+
+    if (connection != null) {
+      outboundConnections.add(connection);
+    }
+
+    return connection;
   }
 }
