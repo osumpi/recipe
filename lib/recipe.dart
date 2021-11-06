5f8c9af5ad94174c6cbda4ba75262e296a353f23
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-06 10:04:39 +0530

    `recipe` library exports `recipe.baker` library

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -4,3 +4,5 @@ export 'src/bake_context.dart' show BakeContext;
 export 'src/recipe.dart' show Recipe;
 export 'src/bake_report.dart' show BakeReport;
 export 'src/utils.dart' show bake, FrameworkUtils, LogLevels, Statuses;
+
+export 'src/baker/baker.dart';
