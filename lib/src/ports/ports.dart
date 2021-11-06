4708a509b83d09f2feb7678c693732b47c4dca73
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-06 13:03:28 +0530

    part port_base

--- lib/src/ports/ports.dart
+++ lib/src/ports/ports.dart
@@ -1,1 +1,8 @@
 library recipe.ports;
+
+import 'package:recipe/src/framework_entity.dart'
+    show FrameworkEntity, EntityLogging;
+
+import 'package:recipe/src/utils.dart' show JsonMap;
+
+part 'src/port_base.dart';
