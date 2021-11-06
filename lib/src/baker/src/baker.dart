abacae03ca936d241b0a6af2426f96dda16f66e9
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-06 11:02:15 +0530

    docs for request bake

--- lib/src/baker/src/baker.dart
+++ lib/src/baker/src/baker.dart
@@ -45,6 +45,13 @@ abstract class Baker<T extends BakerOptions>
   @protected
   Future<BakeReport> bake(BakeContext inputContext);
 
+  /// Requests a bake on [recipe] for a given [inputContext].
+  ///
+  /// The request may be processed immediatley or be queued to be baked at a
+  /// later moment or can be rejected completley.
+  ///
+  /// Refer baker implementation docs to see how the [requestBake] is actually
+  /// handled.
   void requestBake(BakeContext inputContext);
 
   Type get bakerType => runtimeType;
