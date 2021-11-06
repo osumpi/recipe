7da834dafddb0da36bb37e57660c35c4c6364e55
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-06 15:08:39 +0530

    bake context direct support for multi-parent

--- lib/src/bake_context.dart
+++ lib/src/bake_context.dart
@@ -2,6 +2,7 @@ import 'dart:collection' show UnmodifiableMapView;
 
 import 'package:meta/meta.dart';
 import 'package:recipe/src/framework_entity.dart';
+import 'package:recipe/src/utils.dart';
 import 'package:recipe/src/ports/ports.dart';
 import 'package:recipe/src/recipe.dart' show Recipe;
 
@@ -22,46 +23,13 @@ class BakeContext with FrameworkEntity, EntityLogging {
 
   bool get hasParent => parents.isNotEmpty;
 
-  late final path = _findPath();
-
-  late final ancestors = _getVisitationRouteFromRoot();
-
-  String _findPath() {
-    return parentContext == null
-        ? '${recipe.name}'
-        : '${parentContext?.path}/${recipe.name}';
-  }
-
-  T findNearestRecipeOfExactType<T extends Recipe>({
-    T Function()? onNotFound,
-  }) {
-    for (final recipe in ancestors) {
-      if (recipe is T) {
-        return recipe;
-      }
-    }
-
-    if (onNotFound == null) {
-      error("Could not find $T in $ancestors");
-      throw RecipeNotFound<T>(ancestors);
-    }
-
-    return onNotFound();
-  }
-
-  List<Recipe> _getVisitationRouteFromRoot() {
-    final result = [recipe];
-
-    var p = this.parentContext;
-
-    while (p != null) {
-      result.add(p.recipe);
-      p = p.parentContext;
-    }
-
-    return result;
-  }
-
+  // TODO: improve json expansion
   @override
-  String toString() => ancestors.reversed.join(' -> ');
+  JsonMap toJson() {
+    return {
+      ...super.toJson(),
+      'of': recipe.hashCode,
+      'parent context': {...parentContext}
+    };
+  }
 }
