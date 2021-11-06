b8b3e4c7fca5ca0094759a5585a4e20e04e349ea
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-06 19:16:39 +0530

    updates example/test

--- example/test.dart
+++ example/test.dart
@@ -1,8 +1,27 @@
 import 'package:recipe/recipe.dart';
+import 'package:recipe/src/ports/ports.dart';
 
 void main() async {
   FrameworkUtils.loggingLevel = LogLevels.verbose;
   FrameworkUtils.showTimestampInLogs = true;
 
   // bake(MyRecipe()).listen(FrameworkUtils.log);
+}
+
+class MyRecipe extends Recipe {
+  late final InputPort<double> theta;
+
+  @override
+  void initialize() {
+    // Temperory solution until meta is fixed. Try on dart not from flutter-sdk.
+    final theta = inputPort<double>('theta');
+    this.theta = theta;
+
+    super.initialize();
+  }
+
+  @override
+  Future<void> bake(BakeContext<Object> context) {
+    throw UnimplementedError();
+  }
 }
