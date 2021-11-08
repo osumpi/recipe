66ca25d59c210b9c04f280fea6663696abb9601a
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-08 18:36:17 +0530

    clean test with recommended appraoches only

--- example/test.dart
+++ example/test.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
 import 'package:recipe/recipe.dart';
-import 'package:recipe/src/ports/ports.dart';
-import 'package:recipe/src/recipe.dart';
 
 Future<void> main() async {
   FrameworkUtils.loggingLevel = LogLevels.verbose;
@@ -28,8 +26,8 @@ class MySimpleRecipe extends Recipe<int, String> {
 
 // Recipe block that takes two numbers, and yields their quotient and remainder.
 class BitComplexRecipe extends MultiIORecipe {
-  final numeratorPort = MultiInboundInputPort('numerator');
-  final denominatorPort = MultiInboundInputPort('denominator');
+  final numeratorPort = MultiInboundInputPort<int>('numerator');
+  final denominatorPort = MultiInboundInputPort<int>('denominator');
 
   final quotientPort = OutputPort('quotient');
   final remainderPort = OutputPort('remainder');
@@ -41,31 +39,13 @@ class BitComplexRecipe extends MultiIORecipe {
   Set<OutputPort> get outputPorts => {quotientPort, remainderPort};
 
   @override
-  Stream<MuxedOutput> bake(BakeContext<MuxedInputs> context) async* {
-    int numerator, denominator;
-
-    // Input Extraction Approach 1: take inputs from context (NOT RECOMMENDED)
-    numerator = context.data[numeratorPort] as int;
-    denominator = context.data[denominatorPort] as int;
-
-    // Input Extraction Approach 2: take input directly from the port (RECOMMENDED)
-    numerator = numeratorPort.data;
-    denominator = denominatorPort.data;
+  Stream<MuxedOutput> bake(final BakeContext<MuxedInputs> context) async* {
+    final numerator = numeratorPort.data;
+    final denominator = denominatorPort.data;
 
     final quotient = (numerator / denominator).truncate();
     final remainder = numerator % denominator;
 
-    // Output Yielding Approach 1: pack and yield MuxedOutput
-    yield MuxedOutput({
-      quotientPort: quotient,
-      remainderPort: remainder,
-    });
-
-    // Output Yielding Approach 2: asynchronously write to respective ports using context.repackWith
-    quotientPort.write(context.repackWith(data: quotient));
-    remainderPort.write(context.repackWith(data: remainder));
-
-    // Output Yielding Approach 3: asynchronously write to respective ports using MuxedOutputAdapter
     yield* MuxedOutputAdapter.of(context)
       ..writeTo(quotientPort, data: quotient)
       ..writeTo(remainderPort, data: remainder);
