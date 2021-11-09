35182310a069915d68cc98c36996d2a4e0395e3d
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-09 21:21:36 +0530

    fix lints

--- lib/src/baker/src/baker.dart
+++ lib/src/baker/src/baker.dart
@@ -43,7 +43,7 @@ abstract class Baker<T extends BakerOptions>
   bool get canBake;
 
   @protected
-  Future<BakeReport> bake(final BakeContext inputContext);
+  Future<BakeReport> bake(final BakeContext<dynamic> inputContext);
 
   /// Requests a bake on [recipe] for a given [inputContext].
   ///
@@ -52,7 +52,7 @@ abstract class Baker<T extends BakerOptions>
   ///
   /// Refer baker implementation docs to see how the [requestBake] is actually
   /// handled.
-  void requestBake(final BakeContext inputContext);
+  void requestBake(final BakeContext<dynamic> inputContext);
 
   Type get bakerType => runtimeType;
 }
