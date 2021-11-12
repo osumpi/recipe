d7a11d6cb5b77869b7a7f7ba638a395e1cf3e62e
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-12 21:12:26 +0530

    remove log options

--- lib/src/log.dart
+++ lib/src/log.dart
@@ -11,21 +11,17 @@ class Log {
     final Object? object, {
     required final LogLevel level,
     final FrameworkEntity module = anonymous,
-    final LogOptions? logOptions,
   }) =>
-      Log._(level, module, object, logOptions);
+      Log._(level, module, object);
 
   Log._(
-    final this.level,
-    final this.module,
-    final this.object,
-    final LogOptions? _logOptions,
-  )   : logOptions = _logOptions ?? Log.defaultOption,
-        shouldBeep = level == LogLevels.fatal {
-    // Skip remaining if it's not going to be logged.
+    final LogLevel level,
+    final FrameworkEntity module,
+    final Object? object,
+  ) {
     if (level.value < Log.loggingLevel.value) return;
 
-    if (shouldBeep) stdout.writeCharCode(0x07);
+    if (level == LogLevels.fatal) stdout.writeCharCode(0x07);
 
     stdout.writeln(
       "${showTimestamp ? DateTime.now().toString().dim().reset() : ''} ${showLevelSymbolInsteadOfLabel ? level.labelAsSymbol : level.label} $_seperator ${level.moduleNameFormatter(module.name)} $_seperator ${level.messageFormatter(object)}",
@@ -35,84 +31,46 @@ class Log {
   factory Log.fatal(
     final Object? object, {
     final FrameworkEntity module = anonymous,
-    final LogOptions? logOptions,
   }) =>
-      Log._(LogLevels.fatal, module, object, logOptions);
+      Log._(LogLevels.fatal, module, object);
 
   factory Log.error(
     final Object? object, {
     final FrameworkEntity module = anonymous,
-    final LogOptions? logOptions,
   }) =>
-      Log._(LogLevels.error, module, object, logOptions);
+      Log._(LogLevels.error, module, object);
 
   factory Log.warn(
     final Object? object, {
     final FrameworkEntity module = anonymous,
-    final LogOptions? logOptions,
   }) =>
-      Log._(LogLevels.warning, module, object, logOptions);
+      Log._(LogLevels.warning, module, object);
 
   factory Log.info(
     final Object? object, {
     final FrameworkEntity module = anonymous,
-    final LogOptions? logOptions,
   }) =>
-      Log._(LogLevels.info, module, object, logOptions);
+      Log._(LogLevels.info, module, object);
 
   factory Log.verbose(
     final Object? object, {
     final FrameworkEntity module = anonymous,
-    final LogOptions? logOptions,
   }) =>
-      Log._(LogLevels.verbose, module, object, logOptions);
+      Log._(LogLevels.verbose, module, object);
 
   factory Log.trace(
     final Object? object, {
     final FrameworkEntity module = anonymous,
-    final LogOptions? logOptions,
   }) =>
-      Log._(LogLevels.trace, module, object, logOptions);
+      Log._(LogLevels.trace, module, object);
 
   static const _seperator = "•";
-
-  static LogOptions defaultOption = const LogOptions.defaults();
 
   static LogLevel loggingLevel = LogLevels.info;
 
   static bool showLevelSymbolInsteadOfLabel = false;
 
   static bool showTimestamp = false;
-
-  final Object? object;
-  final FrameworkEntity module;
-  final LogLevel level;
-  final LogOptions logOptions;
-  final bool shouldBeep;
-}
-
-@immutable
-class LogOptions {
-  const LogOptions({
-    final this.includeTimestamps = true,
-    final this.applyColors = true,
-  });
-
-  const factory LogOptions.defaults() = LogOptions;
-
-  final bool includeTimestamps;
-
-  final bool applyColors;
-
-  LogOptions copyWith({
-    final bool? includeTimestamps,
-    final bool? applyColors,
-  }) {
-    return LogOptions(
-      includeTimestamps: includeTimestamps ?? this.includeTimestamps,
-      applyColors: applyColors ?? this.applyColors,
-    );
-  }
 }
 
 class _AnonymousModule with FrameworkEntity {
