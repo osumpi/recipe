b8f28cf887f3e46ee377dcc30ce697e846d67aaf
Rithvik Nishad <rithvikn2001@gmail.com>
2022-01-01 14:21:19 +0530

    move exceptions

--- lib/src/foundation/exceptions.dart
+++ lib/src/foundation/exceptions.dart
@@ -4,12 +4,12 @@ import 'recipe.dart';
 
 /// Recipe of type [T] was not found in the [visitedRecipes].
 @immutable
-class RecipeNotFound<T extends Recipe<dynamic, dynamic>> implements Exception {
+class RecipeNotFound<T extends Recipe> implements Exception {
   const RecipeNotFound(this.visitedRecipes);
 
   /// The recipes that were visited and checked against if it was of type [T].
   /// This shall include all the recipes passed by the requester.
-  final List<Recipe<dynamic, dynamic>> visitedRecipes;
+  final List<Recipe> visitedRecipes;
 
   @override
   String toString() {
@@ -20,4 +20,14 @@ Visited: ${visitedRecipes.join(' -> ')}
 This is possibly because there was no recipe of the specified type that was executed before the recipe that requested it.
 Try adding `$T` in the visitation route, or by specifying `onNotFound`.""";
   }
+}
+
+class BakerAccessedBeforeInitializeException implements Exception {
+  const BakerAccessedBeforeInitializeException(this.recipe);
+
+  final Recipe recipe;
+
+  @override
+  String toString() =>
+      'Attempted to access baker of $recipe before initialization of recipe completed.';
 }
